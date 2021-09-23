976b6720dcb4a9af3a9d11da1bbee76bc1781c10
Nathan Reis <nathansilvareis@gmail.com>
2021-09-23 20:48:33 -0300

    EVN-DOCKER: Modificado para apenas ambiente Docker

--- api/src/errors/AppErrors.ts
+++
@@ -1,9 +0,0 @@
-export class AppError {
-  public readonly message: string | Array<string>;
-  public readonly statusCode: number;
-
-  constructor(message: string | Array<string>, statusCode = 400) {
-    this.message = message;
-    this.statusCode = statusCode;
-  }
-}
--- mobile/App.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import Navigator from './src/navigators';
-
-export default function App() {
-  return <Navigator />;
-}
