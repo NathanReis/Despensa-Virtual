9f7f9461a5ef14789c84260077ec3b86c703ca94
Nathan Reis <nathansilvareis@gmail.com>
2021-09-28 16:34:36 -0300

    Adicionado selecao de imagem apartir de galeria

--- src/screens/testOcr/index.tsx
+++ src/screens/testOcr/index.tsx
@@ -1,15 +1,56 @@
-import React from 'react';
+import * as ImagePicker from 'expo-image-picker';
+import React, { useEffect, useState } from 'react';
 import {
-  Text
+  Image
 } from 'react-native';
+import { CustomButton } from '../../components/button';
 import { SafeZoneScreen } from '../../components/safeZoneScreen';
 
 import styles from './style';
 
 export function TestOCR() {
+  let [image, setImage] = useState<String>();
+
+  useEffect(() => {
+    async function checkPermission() {
+      let currentStatus = (await ImagePicker.getMediaLibraryPermissionsAsync()).status;
+
+      if (currentStatus !== 'granted') {
+        let newStatus = (await ImagePicker.requestMediaLibraryPermissionsAsync()).status;
+
+        if (newStatus !== 'granted') {
+          alert('You need to permit that this app access you images');
+        }
+      }
+    }
+
+    checkPermission();
+  }, []);
+
+  async function handlePickImage() {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      allowsMultipleSelection: false,
+      aspect: [1, 1],
+      quality: 1
+    });
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  }
+
   return (
     <SafeZoneScreen>
-      <Text>OCR</Text>
+      {image && <Image
+        style={styles.image}
+        source={{ uri: image }}
+      />}
+      <CustomButton
+        title='Selecionar imagem'
+        onPress={handlePickImage}
+      />
     </SafeZoneScreen>
   );
 }
--- src/screens/testOcr/style.ts
+++ src/screens/testOcr/style.ts
@@ -1,3 +1,10 @@
 import { StyleSheet } from 'react-native';
 
-export default StyleSheet.create({});
+export default StyleSheet.create({
+  image: {
+    height: 200,
+    width: 200,
+
+    alignSelf: 'center'
+  }
+});
