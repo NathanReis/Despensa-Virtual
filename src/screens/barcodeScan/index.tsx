0b01f7f74e33744e3140150c99af52f6e1376d3a
Nathan Reis <nathansilvareis@gmail.com>
2022-02-13 14:36:08 -0300

    Simplifica código de verificar/requisitar permissão da página BarcodeScan e usa novos componentes WaitingPermission e PermissionDenied

--- src/screens/barcodeScan/index.tsx
+++ src/screens/barcodeScan/index.tsx
@@ -1,13 +1,15 @@
 import { useNavigation } from '@react-navigation/core';
 import { BarCodeScanner, BarCodeScannerResult } from 'expo-barcode-scanner';
 import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet, Button, Alert } from 'react-native';
+import { Text, View, StyleSheet, Button } from 'react-native';
 import BarcodeMask from 'react-native-barcode-mask';
 import { TouchableOpacity } from 'react-native-gesture-handler';
+import { PermissionDenied } from '../../components/permissionDenied';
+import { WaitingPermission } from '../../components/waitingPermission';
 import styles from './styles';
 
 export function BarcodeScan() {
-  let [hasPermission, setHasPermission] = useState<boolean>(false);
+  let [hasPermission, setHasPermission] = useState<boolean | null>(null);
   let [scanned, setScanned] = useState<boolean>(false);
   let navigator = useNavigation();
 
@@ -15,16 +17,12 @@ export function BarcodeScan() {
     async function checkPermission() {
       let currentStatus = (await BarCodeScanner.getPermissionsAsync()).status;
 
-      if (currentStatus !== 'granted') {
+      if (currentStatus === 'granted') {
+        setHasPermission(true);
+      } else {
         let newStatus = (await BarCodeScanner.requestPermissionsAsync()).status;
 
-        if (newStatus !== 'granted') {
-          Alert.alert('Você precisa permitir acesso à câmera para usar este app');
-        } else {
-          setHasPermission(true);
-        }
-      } else {
-        setHasPermission(true);
+        setHasPermission(newStatus === 'granted');
       }
     }
 
@@ -36,8 +34,10 @@ export function BarcodeScan() {
     navigator.navigate('Product' as never, { barcode: data } as never);
   };
 
-  if (!hasPermission) {
-    return <Text>Precisamos de sua permissão</Text>;
+  if (hasPermission === null) {
+    return <WaitingPermission />;
+  } else if (!hasPermission) {
+    return <PermissionDenied resource='Câmera' />;
   }
 
   return (
