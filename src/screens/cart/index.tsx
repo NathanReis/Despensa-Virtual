b659aa3d5fe075af8ca7a1d32f1476a5c406587c
Nathan Reis <nathansilvareis@gmail.com>
2022-03-27 15:46:33 -0300

    Corrige local da importação do UserGroupModel em CartScreen e PantryScreen

--- src/screens/cart/index.tsx
+++ src/screens/cart/index.tsx
@@ -7,158 +7,159 @@ import { CustomButton } from '../../components/button';
 import { Loading } from '../../components/loading';
 import api from '../../services/api';
 import { Purchase } from '../../storage/Purchase';
-import { IUserGroupModel, IUserModel, User } from '../../storage/User';
+import { IUserModel, User } from '../../storage/User';
+import { IUserGroupModel } from '../../storage/UserGroup';
 import { IProductModel } from '../product/IProductModel';
 import styles from './styles';
 
 export function Cart() {
 
-    let [products, setProducts] = useState<IProductModel[]>([]);
-    let [loggedUser, setLoggedUser] = useState<IUserModel>({} as IUserModel);
-    let [defaultUserGroup, setDefaultUserGroup] = useState<IUserGroupModel>({} as IUserGroupModel);
-    let [isLoading, setIsLoading] = useState<boolean>(true);
-    let [defaultUserGroupId, setDefaultUserGroupId] = useState<number>(0);
-    let navigator = useNavigation();
+  let [products, setProducts] = useState<IProductModel[]>([]);
+  let [loggedUser, setLoggedUser] = useState<IUserModel>({} as IUserModel);
+  let [defaultUserGroup, setDefaultUserGroup] = useState<IUserGroupModel>({} as IUserGroupModel);
+  let [isLoading, setIsLoading] = useState<boolean>(true);
+  let [defaultUserGroupId, setDefaultUserGroupId] = useState<number>(0);
+  let navigator = useNavigation();
 
-    const isFocused = useIsFocused();
-    useEffect(() => {
-        if (isFocused) {
-            async function load() {
-                setIsLoading(true);
+  const isFocused = useIsFocused();
+  useEffect(() => {
+    if (isFocused) {
+      async function load() {
+        setIsLoading(true);
 
-                let products = await Purchase.getProducts();
-                setProducts(products);
+        let products = await Purchase.getProducts();
+        setProducts(products);
 
-                let user = await User.getLoggedUser();
-                setLoggedUser(user);
-                let defaultGroup;
+        let user = await User.getLoggedUser();
+        setLoggedUser(user);
+        let defaultGroup;
 
-                if (user.idDefaultUserGroup != null) {
-                    defaultGroup = user.userGroupEntities.find(x => x.id == user.idDefaultUserGroup);
-                    setDefaultUserGroup(defaultGroup!);
-                    setDefaultUserGroupId(defaultGroup!.id);
-                }
-
-                setIsLoading(false);
-            }
-
-            load().catch(error => {
-                Alert.alert('Erro', JSON.stringify(error));
-            });
+        if (user.idDefaultUserGroup != null) {
+          defaultGroup = user.userGroupEntities.find(x => x.id == user.idDefaultUserGroup);
+          setDefaultUserGroup(defaultGroup!);
+          setDefaultUserGroupId(defaultGroup!.id);
         }
 
-    }, [isFocused]);
+        setIsLoading(false);
+      }
 
-    if (isLoading) {
-        return <Loading />;
-    }
-    if (defaultUserGroupId == 0) {
-        return (
-            <View >
-                <Text style={styles.pageTitle}>Você precisa registrar uma residência para poder salvar seus produtos</Text>
-            </View>
-        )
-    }
-    async function saveProducts() {
-        let productsDTO: any = [];
-
-        products.map(x => {
-            let product = {
-                idProduct: x.id,
-                idUserGroup: defaultUserGroup.id,
-                amount: x.amount,
-                validate: x.validate,
-                measure: {
-                    unit: 'kg',
-                    value: 2
-                },
-                position: x.id
-            }
-
-            productsDTO.push(product);
-        });
-        let date = new Date();
-        let formatedDate = date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear();
-
-        let purchase = {
-            idUserGroup: defaultUserGroupId,
-            products: productsDTO,
-            purchaseDate: formatedDate
-        }
-        try {
-            await api.post('/purchases', purchase);
-            Alert.alert('Produtos salvos com sucesso!')
-            Purchase.cleanProducts();
-            setProducts([]);
-        }
-        catch (error: any) {
-            console.log(error.response)
-            Alert.alert(error.response.data.error[0])
-        }
+      load().catch(error => {
+        Alert.alert('Erro', JSON.stringify(error));
+      });
     }
 
-    function convertStringToDate(dateStr: string): Date {
-        let dates = dateStr.split('/');
-        let date = new Date()
-        date.setDate(Number(dates[0]));
-        date.setMonth(Number(dates[1]) - 1);
-        date.setFullYear(Number(dates[2]));
+  }, [isFocused]);
 
-        return date;
-    }
-
-    async function removeProductFromCart(barcode: string) {
-        let newProducts = await Purchase.removeProductByBarcode(barcode);
-        setProducts(newProducts);
-    }
-
+  if (isLoading) {
+    return <Loading />;
+  }
+  if (defaultUserGroupId == 0) {
     return (
-        <View style={styles.container}>
-            <Text style={styles.pageTitle}>Salvar Produtos</Text>
-
-            <ScrollView >
-                {products.map(x =>
-                (
-                    <View key={x.id} style={styles.productContainer}>
-                        <View style={styles.productImgContainer}>
-                            <Image style={styles.image} source={require('../../../assets/logo.png')} />
-                            <View style={styles.productNameContainer}>
-                                <Text style={styles.productName}>{x.name}</Text>
-                            </View>
-                            <CustomButton
-                                onPress={() => removeProductFromCart(x.barcode)}
-                            >
-                                <FontAwesome size={30} style={styles.trashIcon} name='trash' />
-                            </CustomButton>
-                        </View>
-
-
-                        <Text>{x.amount} x</Text>
-                        {/* <Text>Vence em: {x.validate}</Text> */}
-                    </View>
-
-                ))}
-            </ScrollView>
-            <View style={styles.userGroupContainer}>
-                <View style={styles.pickerBorder}>
-                    <Picker
-                        selectedValue={defaultUserGroupId}
-                        onValueChange={(value, index) => setDefaultUserGroupId(value)}
-                        mode='dropdown' // Android only
-                        style={styles.picker}
-                    >
-                        {/* <Picker.Item label={defaultUserGroup.name} value={defaultUserGroup.id} /> */}
-                        {loggedUser.userGroupEntities.map(x => (
-                            <Picker.Item key={x.id} label={x.name} value={x.id} />
-                        ))}
-                    </Picker>
-                </View>
-
-                {products.length > 0 &&
-                    <CustomButton onPress={saveProducts} style={styles.btnAdicionar}>
-                        <Text style={styles.btnAdicionarTxt}>Adicionar</Text>
-                    </CustomButton>}
-            </View>
-        </View>
+      <View >
+        <Text style={styles.pageTitle}>Você precisa registrar uma residência para poder salvar seus produtos</Text>
+      </View>
     )
+  }
+  async function saveProducts() {
+    let productsDTO: any = [];
+
+    products.map(x => {
+      let product = {
+        idProduct: x.id,
+        idUserGroup: defaultUserGroup.id,
+        amount: x.amount,
+        validate: x.validate,
+        measure: {
+          unit: 'kg',
+          value: 2
+        },
+        position: x.id
+      }
+
+      productsDTO.push(product);
+    });
+    let date = new Date();
+    let formatedDate = date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear();
+
+    let purchase = {
+      idUserGroup: defaultUserGroupId,
+      products: productsDTO,
+      purchaseDate: formatedDate
+    }
+    try {
+      await api.post('/purchases', purchase);
+      Alert.alert('Produtos salvos com sucesso!')
+      Purchase.cleanProducts();
+      setProducts([]);
+    }
+    catch (error: any) {
+      console.log(error.response)
+      Alert.alert(error.response.data.error[0])
+    }
+  }
+
+  function convertStringToDate(dateStr: string): Date {
+    let dates = dateStr.split('/');
+    let date = new Date()
+    date.setDate(Number(dates[0]));
+    date.setMonth(Number(dates[1]) - 1);
+    date.setFullYear(Number(dates[2]));
+
+    return date;
+  }
+
+  async function removeProductFromCart(barcode: string) {
+    let newProducts = await Purchase.removeProductByBarcode(barcode);
+    setProducts(newProducts);
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.pageTitle}>Salvar Produtos</Text>
+
+      <ScrollView >
+        {products.map(x =>
+        (
+          <View key={x.id} style={styles.productContainer}>
+            <View style={styles.productImgContainer}>
+              <Image style={styles.image} source={require('../../../assets/logo.png')} />
+              <View style={styles.productNameContainer}>
+                <Text style={styles.productName}>{x.name}</Text>
+              </View>
+              <CustomButton
+                onPress={() => removeProductFromCart(x.barcode)}
+              >
+                <FontAwesome size={30} style={styles.trashIcon} name='trash' />
+              </CustomButton>
+            </View>
+
+
+            <Text>{x.amount} x</Text>
+            {/* <Text>Vence em: {x.validate}</Text> */}
+          </View>
+
+        ))}
+      </ScrollView>
+      <View style={styles.userGroupContainer}>
+        <View style={styles.pickerBorder}>
+          <Picker
+            selectedValue={defaultUserGroupId}
+            onValueChange={(value, index) => setDefaultUserGroupId(value)}
+            mode='dropdown' // Android only
+            style={styles.picker}
+          >
+            {/* <Picker.Item label={defaultUserGroup.name} value={defaultUserGroup.id} /> */}
+            {loggedUser.userGroupEntities.map(x => (
+              <Picker.Item key={x.id} label={x.name} value={x.id} />
+            ))}
+          </Picker>
+        </View>
+
+        {products.length > 0 &&
+          <CustomButton onPress={saveProducts} style={styles.btnAdicionar}>
+            <Text style={styles.btnAdicionarTxt}>Adicionar</Text>
+          </CustomButton>}
+      </View>
+    </View>
+  )
 }
--- src/screens/pantry/index.tsx
+++ src/screens/pantry/index.tsx
@@ -1,160 +1,161 @@
 import { Picker } from '@react-native-picker/picker';
 import { useIsFocused, useNavigation } from '@react-navigation/native';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Image, SafeAreaView, ScrollView, Text, TextInput, View } from 'react-native';
 import { CustomButton } from '../../components/button';
 import { Loading } from '../../components/loading';
 import api from '../../services/api';
-import { IUserGroupModel, IUserModel, User } from '../../storage/User';
+import { IUserModel, User } from '../../storage/User';
+import { IUserGroupModel } from '../../storage/UserGroup';
 import { IMyProductModel } from '../product/IMyProductModel';
 import styles from './styles';
 
 export function Pantry() {
 
-    let [products, setProducts] = useState<IMyProductModel[]>([]);
-    let [loggedUser, setLoggedUser] = useState<IUserModel>({} as IUserModel);
-    let [defaultUserGroup, setDefaultUserGroup] = useState<IUserGroupModel>({} as IUserGroupModel);
-    let [defaultUserGroupId, setDefaultUserGroupId] = useState<number>(0);
-    let [searchedProducts, setSearchedProducts] = useState<IMyProductModel[]>([]);
-    let [isLoading, setIsLoading] = useState<boolean>(true);
-    let navigator = useNavigation();
+  let [products, setProducts] = useState<IMyProductModel[]>([]);
+  let [loggedUser, setLoggedUser] = useState<IUserModel>({} as IUserModel);
+  let [defaultUserGroup, setDefaultUserGroup] = useState<IUserGroupModel>({} as IUserGroupModel);
+  let [defaultUserGroupId, setDefaultUserGroupId] = useState<number>(0);
+  let [searchedProducts, setSearchedProducts] = useState<IMyProductModel[]>([]);
+  let [isLoading, setIsLoading] = useState<boolean>(true);
+  let navigator = useNavigation();
 
-    const isFocused = useIsFocused();
-    useEffect(() => {
-        if (isFocused) {
-            async function load() {
-                setIsLoading(true);
-                let user = await User.getLoggedUser();
-                setLoggedUser(user);
+  const isFocused = useIsFocused();
+  useEffect(() => {
+    if (isFocused) {
+      async function load() {
+        setIsLoading(true);
+        let user = await User.getLoggedUser();
+        setLoggedUser(user);
 
-                let products;
-                let defaultGroup;
+        let products;
+        let defaultGroup;
 
-                if (user.idDefaultUserGroup != null) {
-                    setDefaultUserGroupId(user.idDefaultUserGroup);
-                    products = await api.get<IMyProductModel[]>(`/my-products/${user.idDefaultUserGroup}`);
-                    defaultGroup = user.userGroupEntities.find(x => x.id == user.idDefaultUserGroup);
-                    setProducts(products.data);
-                    setSearchedProducts(products.data);
-                    setDefaultUserGroup(defaultGroup!);
-                    setDefaultUserGroupId(defaultGroup!.id);
-                }
-                setIsLoading(false);
-            }
-
-            load().catch(error => {
-                console.log(error.response.data)
-                // Alert.alert('Erro', JSON.stringify(error.response.data));
-                setIsLoading(false);
-
-            });
+        if (user.idDefaultUserGroup != null) {
+          setDefaultUserGroupId(user.idDefaultUserGroup);
+          products = await api.get<IMyProductModel[]>(`/my-products/${user.idDefaultUserGroup}`);
+          defaultGroup = user.userGroupEntities.find(x => x.id == user.idDefaultUserGroup);
+          setProducts(products.data);
+          setSearchedProducts(products.data);
+          setDefaultUserGroup(defaultGroup!);
+          setDefaultUserGroupId(defaultGroup!.id);
         }
+        setIsLoading(false);
+      }
 
-    }, [isFocused]);
+      load().catch(error => {
+        console.log(error.response.data)
+        // Alert.alert('Erro', JSON.stringify(error.response.data));
+        setIsLoading(false);
 
-    async function handleChangeUserGroup(id: number) {
-
-        setDefaultUserGroupId(id);
-        try {
-            let products = await api.get<IMyProductModel[]>(`/my-products/${id}`);
-            setProducts(products.data);
-            setSearchedProducts(products.data)
-        } catch (error) {
-            setProducts([])
-            setSearchedProducts([])
-        }
-
+      });
     }
 
-    function handleSearchProduct(productName: string) {
-        let possibleProducts = products.filter(x => x.productEntity.name.toLocaleLowerCase().indexOf(productName.toLocaleLowerCase()) > -1)
+  }, [isFocused]);
 
-        if (possibleProducts.length > 0)
-            setSearchedProducts(possibleProducts);
-        else
-            setSearchedProducts(products)
+  async function handleChangeUserGroup(id: number) {
+
+    setDefaultUserGroupId(id);
+    try {
+      let products = await api.get<IMyProductModel[]>(`/my-products/${id}`);
+      setProducts(products.data);
+      setSearchedProducts(products.data)
+    } catch (error) {
+      setProducts([])
+      setSearchedProducts([])
     }
 
-    if (isLoading) {
-        return <Loading />;
-    }
+  }
 
-    if (defaultUserGroupId == 0) {
-        return (
-            <View style={{ padding: 10 }}>
-                <Text style={styles.pageTitle}>Você precisa registrar uma residência para visualizar seus produtos!</Text>
-            </View>
-        )
-    }
+  function handleSearchProduct(productName: string) {
+    let possibleProducts = products.filter(x => x.productEntity.name.toLocaleLowerCase().indexOf(productName.toLocaleLowerCase()) > -1)
 
-    if (searchedProducts.length == 0) {
-        return (
-            <View style={{ padding: 10 }}>
-                <Text style={styles.pageTitle}>Você não possui nenhum produto nessa despensa</Text>
-                <SafeAreaView style={styles.userGroupContainer}>
-                    <View style={styles.pickerBorder}>
-                        <Picker
-                            selectedValue={defaultUserGroupId}
-                            onValueChange={(value, index) => handleChangeUserGroup(value)}
-                            mode='dropdown' // Android only
-                            style={styles.picker}
-                        >
-                            {loggedUser.userGroupEntities.map(x => (
-                                <Picker.Item key={x.id} label={x.name} value={x.id} />
-                            ))}
-                        </Picker>
+    if (possibleProducts.length > 0)
+      setSearchedProducts(possibleProducts);
+    else
+      setSearchedProducts(products)
+  }
 
-                    </View>
+  if (isLoading) {
+    return <Loading />;
+  }
 
-                </SafeAreaView>
-            </View>
-        )
-    }
-
+  if (defaultUserGroupId == 0) {
     return (
-        <View style={styles.container}>
-            <Text style={styles.pageTitle}>Sua Despensa</Text>
-            <View style={styles.searchContainer}>
-                <TextInput onChangeText={(productName) => handleSearchProduct(productName)} placeholder='Buscar Produto' style={styles.searchInput} />
-                <CustomButton style={styles.menuItemButton}>
-                    <Image
-                        style={styles.iconContainer}
-                        source={require('../../../assets/searchIcon.png')}
-                    />
-                </CustomButton>
-            </View>
-
-            <ScrollView>
-                {searchedProducts.map(x =>
-                (
-                    <View key={x.id} style={styles.productContainer}>
-                        <View style={styles.productImgContainer}>
-                            <Image style={styles.image} source={require('../../../assets/logo.png')} />
-                            <View style={styles.productNameContainer}>
-                                <Text style={styles.productName}>{x.productEntity.name}</Text>
-                                <Text style={styles.productName}>Vence em: {x.validate}</Text>
-                            </View>
-                        </View>
-                        <Text>{x.amount} x</Text>
-                    </View>
-                ))}
-            </ScrollView>
-            <SafeAreaView style={styles.userGroupContainer}>
-                <View style={styles.pickerBorder}>
-                    <Picker
-                        selectedValue={defaultUserGroupId}
-                        onValueChange={(value, index) => handleChangeUserGroup(value)}
-                        mode='dropdown' // Android only
-                        style={styles.picker}
-                    >
-                        {loggedUser.userGroupEntities.map(x => (
-                            <Picker.Item key={x.id} label={x.name} value={x.id} />
-                        ))}
-                    </Picker>
-
-                </View>
-
-            </SafeAreaView>
-        </View>
+      <View style={{ padding: 10 }}>
+        <Text style={styles.pageTitle}>Você precisa registrar uma residência para visualizar seus produtos!</Text>
+      </View>
     )
+  }
+
+  if (searchedProducts.length == 0) {
+    return (
+      <View style={{ padding: 10 }}>
+        <Text style={styles.pageTitle}>Você não possui nenhum produto nessa despensa</Text>
+        <SafeAreaView style={styles.userGroupContainer}>
+          <View style={styles.pickerBorder}>
+            <Picker
+              selectedValue={defaultUserGroupId}
+              onValueChange={(value, index) => handleChangeUserGroup(value)}
+              mode='dropdown' // Android only
+              style={styles.picker}
+            >
+              {loggedUser.userGroupEntities.map(x => (
+                <Picker.Item key={x.id} label={x.name} value={x.id} />
+              ))}
+            </Picker>
+
+          </View>
+
+        </SafeAreaView>
+      </View>
+    )
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.pageTitle}>Sua Despensa</Text>
+      <View style={styles.searchContainer}>
+        <TextInput onChangeText={(productName) => handleSearchProduct(productName)} placeholder='Buscar Produto' style={styles.searchInput} />
+        <CustomButton style={styles.menuItemButton}>
+          <Image
+            style={styles.iconContainer}
+            source={require('../../../assets/searchIcon.png')}
+          />
+        </CustomButton>
+      </View>
+
+      <ScrollView>
+        {searchedProducts.map(x =>
+        (
+          <View key={x.id} style={styles.productContainer}>
+            <View style={styles.productImgContainer}>
+              <Image style={styles.image} source={require('../../../assets/logo.png')} />
+              <View style={styles.productNameContainer}>
+                <Text style={styles.productName}>{x.productEntity.name}</Text>
+                <Text style={styles.productName}>Vence em: {x.validate}</Text>
+              </View>
+            </View>
+            <Text>{x.amount} x</Text>
+          </View>
+        ))}
+      </ScrollView>
+      <SafeAreaView style={styles.userGroupContainer}>
+        <View style={styles.pickerBorder}>
+          <Picker
+            selectedValue={defaultUserGroupId}
+            onValueChange={(value, index) => handleChangeUserGroup(value)}
+            mode='dropdown' // Android only
+            style={styles.picker}
+          >
+            {loggedUser.userGroupEntities.map(x => (
+              <Picker.Item key={x.id} label={x.name} value={x.id} />
+            ))}
+          </Picker>
+
+        </View>
+
+      </SafeAreaView>
+    </View>
+  )
 }
