bf12f32c146098f4aee7a453ad76d2711490a78e
Nathan Reis <nathansilvareis@gmail.com>
2022-03-27 15:43:06 -0300

    Remove model de UserGroup do storage de User

--- src/storage/User.ts
+++ src/storage/User.ts
@@ -1,27 +1,23 @@
 import { LocalStorageHelper } from '../helpers/LocalStorageHelper';
+import { IUserGroupModel } from './UserGroup';
 
 export interface IUserModel {
-    email: string,
-    id: number,
-    idDefaultUserGroup: number,
-    name: string,
-    picture: string,
-    userGroupEntities: Array<IUserGroupModel>
+  email: string,
+  id: number,
+  idDefaultUserGroup: number | null,
+  name: string,
+  picture: string,
+  userGroupEntities: Array<IUserGroupModel>
 }
 
-export interface IUserGroupModel {
-    id: number,
-    name: string
-}
 export class User {
-    public static async getLoggedUser(): Promise<IUserModel> {
-        let json = await LocalStorageHelper.get('loggedUser') || '';
+  public static async getLoggedUser(): Promise<IUserModel> {
+    let json = await LocalStorageHelper.get('loggedUser') || '';
 
-        return JSON.parse(json);
-    }
+    return JSON.parse(json);
+  }
 
-    public static async setLoggedUser(user: IUserModel) {
-        await LocalStorageHelper.set('loggedUser', JSON.stringify(user));
-    }
-
+  public static async setLoggedUser(user: IUserModel) {
+    await LocalStorageHelper.set('loggedUser', JSON.stringify(user));
+  }
 }
