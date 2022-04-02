7b5f6ec2c2b3935e20d3a281529476801313fe66
Nathan Reis <nathansilvareis@gmail.com>
2022-04-02 20:26:35 -0300

    Adiciona cabeçalho de idioma na API e OCR

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 let api = axios.create({
-  baseURL: process.env.API_URL
-  // baseURL: 'http://192.168.0.122:3000'
+  baseURL: process.env.API_URL,
+  headers: { 'Accept-Language': 'pt-br' }
 });
 
 export default api;
--- src/services/ocr.ts
+++ src/services/ocr.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 let ocr = axios.create({
-  baseURL: process.env.OCR_URL
+  baseURL: process.env.OCR_URL,
+  headers: { 'Accept-Language': 'pt-br' }
 });
 
 export default ocr;
